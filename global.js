2d07b5185f6c4676e8fe10284c9db5cf130b0d1b
ragingkamoteque <143064824+ragingkamoteque@users.noreply.github.com>
2023-12-14 05:44:01 -0700

    Add files via upload

---
+++ global.js
@@ -0,0 +1,18 @@
+let movement = 0;
+let cardGroup = [["🐶", "🦊"], ["🐨", "🦁"], ["🐯", "🙊"]];
+
+let levelActual = 0;
+let levels = [
+    {
+        cards: cardGroup[0],
+        movementMax: 4
+    },
+    {
+        cards: cardGroup[0].concat(cardGroup[1]),
+        movementMax: 10
+    },
+    {
+        cards: cardGroup[0].concat(cardGroup[1],       cardGroup[2]),
+        movementMax: 14
+    }
+];
---
+++ timer.js
@@ -0,0 +1,35 @@
+function countDown() {
+    let seconds = 10;
+    let minutes = 0;
+    let secondsText;
+    let minutesText;
+    let stopwatch;
+    
+    
+   function startCounter() {
+       seconds--;
+       if(seconds < 0) {
+           seconds = 59;
+           minutes--;
+       }
+       if(minutes < 0) {
+           seconds = 0;
+           minutes=0;
+           clearInterval(stopwatch);
+       }
+       secondsText = seconds;
+       minutesText = minutesText;
+       if(seconds < 10) {
+           secondsText = "0" + seconds;
+       }
+       if(minutes < 10) {
+           minutesText = "0" + minutes;
+       }
+       
+        document.querySelector("#minutes").innerText = minutesText;
+        document.querySelector("#seconds").innerText = secondsText; 
+   }
+   
+    
+  stopwatch = setInterval(startCounter, 1000); 
+}
